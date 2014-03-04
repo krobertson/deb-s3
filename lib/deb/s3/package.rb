8d6c94ae775a768d7286e5dbeb96d9d5bb736625
Ken Robertson <ken@invalidlogic.com>
2014-03-04 08:18:30 -0800

    Fix redirection of stderr on 'which dpkg'

--- lib/deb/s3/package.rb
+++ lib/deb/s3/package.rb
@@ -57,7 +57,7 @@ class Deb::S3::Package
     end
 
     def extract_control(package)
-      if system("which dpkg 2>&1 >/dev/null")
+      if system("which dpkg &> /dev/null")
         `dpkg -f #{package}`
       else
         # ar fails to find the control.tar.gz tarball within the .deb
